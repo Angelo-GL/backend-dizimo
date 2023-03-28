c6697aec3970d5c4da25192b230c54ede3ca1ebf
Ângelo Gabriel <angelogabriel207@outlook.com>
2023-03-28 16:24:44 -0300

    Controller User

---
+++ src/main/java/com/dizimo/backend_dizimo/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.dizimo.backend_dizimo.controller;
+
+import com.dizimo.backend_dizimo.dto.MessageResposeDTO;
+import com.dizimo.backend_dizimo.entities.User;
+import com.dizimo.backend_dizimo.exceptions.UserNotFoundExceptions;
+import com.dizimo.backend_dizimo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResposeDTO createUser(@RequestBody @Validated User user){
+        return userService.createUser(user);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll (){
+        return ResponseEntity.ok(userService.findAllUser());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById (@PathVariable Long id) throws UserNotFoundExceptions {
+        User user = userService.findByIdUser(id);
+        return ResponseEntity.ok(user);
+    }
+
+    public MessageResposeDTO UpdateUser (@RequestBody @Validated User user){
+
+    }
+
+}
