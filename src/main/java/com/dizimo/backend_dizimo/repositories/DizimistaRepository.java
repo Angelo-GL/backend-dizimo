0fa73768874c4ee4829be2e5b6b728f4d1f51117
Ângelo Gabriel <angelogabriel207@outlook.com>
2023-03-31 11:27:58 -0300

    Criação interface DizimistaRepository

---
+++ src/main/java/com/dizimo/backend_dizimo/repositories/DizimistaRepository.java
@@ -0,0 +1,7 @@
+package com.dizimo.backend_dizimo.repositories;
+
+import com.dizimo.backend_dizimo.entities.Dizimista;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DizimistaRepository extends JpaRepository<Dizimista, Long> {
+}
