0ed3bf030b7ef4fb4d744386048b770246e215e8
Ângelo Gabriel <angelogabriel207@outlook.com>
2023-04-14 11:08:01 -0300

    Rotas de listar dizimistas e listar dizimista por id

--- src/main/java/com/dizimo/backend_dizimo/controller/DizimistaController.java
+++ src/main/java/com/dizimo/backend_dizimo/controller/DizimistaController.java
@@ -2,14 +2,18 @@ package com.dizimo.backend_dizimo.controller;
 
 import com.dizimo.backend_dizimo.dto.MessageResposeDTO;
 import com.dizimo.backend_dizimo.entities.Dizimista;
+import com.dizimo.backend_dizimo.exceptions.UserNotFoundExceptions;
 import com.dizimo.backend_dizimo.service.DizimistaService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping(value = "/dizimista")
+@RequestMapping(value = "/dizimistas")
 public class DizimistaController {
     @Autowired
     private DizimistaService dizimistaService;
@@ -19,6 +23,19 @@ public class DizimistaController {
     public MessageResposeDTO createDizimista(@RequestBody @Valid Dizimista dizimista){
         return dizimistaService.createDizimista(dizimista);
     }
+
+    @GetMapping
+    public ResponseEntity<List<Dizimista>> findAll () {
+        return ResponseEntity.ok(dizimistaService.findAllDizimista());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Dizimista> findById (@PathVariable Long id) throws UserNotFoundExceptions {
+        return ResponseEntity.ok(dizimistaService.findByIdDizimista(id));
+    }
+
+
+
 
 
 }
