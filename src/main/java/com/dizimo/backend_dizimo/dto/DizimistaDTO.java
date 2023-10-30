9eeabb7b3f9f372d826c2f2f580c300b71f71a06
Angelo Gabriel <angelogabriel207@outlook.com>
2023-10-30 14:01:25 -0300

    Ajuste e criação de DizimistaDTO

---
+++ src/main/java/com/dizimo/backend_dizimo/dto/DizimistaDTO.java
@@ -0,0 +1,91 @@
+package com.dizimo.backend_dizimo.dto;
+
+import com.dizimo.backend_dizimo.entities.Oferta;
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DizimistaDTO {
+    @NotBlank
+    private String name;
+    @NotBlank
+    private LocalDate nascimento;
+    @NotBlank
+    private String bairro;
+    @NotBlank
+    private String rua;
+    private Integer numero;
+    private Boolean ativo;
+    @NotBlank
+    private String celular;
+    private List<Oferta> ofertas = new ArrayList<>();
+
+    public DizimistaDTO() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(LocalDate nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public List<Oferta> getOfertas() {
+        return ofertas;
+    }
+
+    public void setOfertas(List<Oferta> ofertas) {
+        this.ofertas = ofertas;
+    }
+}
